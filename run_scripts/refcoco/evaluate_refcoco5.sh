c869cd2896be3a340c0e72eba36282eb170edf93
Guy <49041568+25icecreamflavors@users.noreply.github.com>
2022-12-19 05:21:17 +0300

    Create evaluate_refcoco5.sh

---
+++ run_scripts/refcoco/evaluate_refcoco5.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# The port for communication. Note that if you want to run multiple tasks on the same machine,
+# you need to specify different port numbers.
+export MASTER_PORT=6082
+export CUDA_VISIBLE_DEVICES=0
+export GPUS_PER_NODE=1
+
+
+########################## Evaluate Refcoco ##########################
+user_dir=../../ofa_module
+bpe_dir=../../utils/BPE
+selected_cols=0,4,2,3
+
+data=../../dataset/refcoco/test_vg.tsv
+path=../../checkpoints/checkpoint.clean_best_score_0.8160.pt
+result_path=../../results/refcoco
+split='vg_answer5.tsv'
+
+python3 -m torch.distributed.launch --nproc_per_node=${GPUS_PER_NODE} --master_port=${MASTER_PORT} ../../evaluate.py \
+    ${data} \
+    --path=${path} \
+    --user-dir=${user_dir} \
+    --task=refcoco \
+    --batch-size=16 \
+    --log-format=simple --log-interval=10 \
+    --seed=7 \
+    --gen-subset=${split} \
+    --results-path=${result_path} \
+    --beam=5 \
+    --min-len=4 \
+    --max-len-a=0 \
+    --max-len-b=4 \
+    --no-repeat-ngram-size=3 \
+    --fp16 \
+    --num-workers=0 \
+    --model-overrides="{\"data\":\"${data}\",\"bpe_dir\":\"${bpe_dir}\",\"selected_cols\":\"${selected_cols}\"}"
