9dbae115033e95ffc2d85108adf00c9cd55e7885
Guy <49041568+25icecreamflavors@users.noreply.github.com>
2022-12-19 06:24:45 +0300

    Update evaluate_refcoco2.sh

--- run_scripts/refcoco/evaluate_refcoco2.sh
+++ run_scripts/refcoco/evaluate_refcoco2.sh
@@ -13,7 +13,7 @@ bpe_dir=../../utils/BPE
 selected_cols=0,4,2,3
 
 data=../../dataset/refcoco/test_vg.tsv
-path=../../checkpoints/checkpoint.clean_best_score_0.8160.pt
+path=../../checkpoints/checkpoint.ofa8150.pt
 result_path=../../results/refcoco
 split='vg_answer2.tsv'
 
